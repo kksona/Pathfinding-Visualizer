2e2593ecd76d1818ef479ee407891303af78843d
Kshitij Kumar <kksona2004@gmail.com>
2025-05-23 17:17:42 +0530

    fixed screen overflows

--- src/App.tsx
+++ src/App.tsx
@@ -11,16 +11,18 @@ function App() {
 
   return (
     <>
+    <div className="h-screen w-full flex items-center justify-center text-white overflow-x-hidden overflow-y-hidden">
       <PathfindingProvider>
         <TileProvider>
           <SpeedProvider>
-            <div className='h-screen w-screen flex flex-col'>
+            <div className='h-screen w-screen flex flex-col overflow-hidden'>
               <Nav isVisualizationRunningRef={isVisualizationRunningRef}/>
               <Grid isVisualizationRunningRef={isVisualizationRunningRef}/>
             </div>
           </SpeedProvider>
         </TileProvider>
       </PathfindingProvider>
+      </div>
     </>
   )
 }
